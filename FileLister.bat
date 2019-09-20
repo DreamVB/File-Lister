856bcfda345a09483448202040a11101003d5f7b
Ben <dreamvb@outlook.com>
2019-09-20 20:16:11 +0100

    Add files via upload

--- FileLister.bat
+++ FileLister.bat
@@ -1,7 +1,5 @@
 rem display a list of files in windows and push the results to a html file.
 @echo off
 cls
-py FileLister.py c:\windows\*.exe > c:\out\dir.html
-rem display the page in the default web broswer
-c:\out\dir.html
+py test.py c:\windows\system32\*.dll output.html
 pause
